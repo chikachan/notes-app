eb476f777f39bd3572b3d4ddb80d60aeab2793cd
Javier <javucho1000@yahoo.com.mx>
2013-10-30 02:02:34 +0900

    Localizing calendar

--- src/main/java/tabitabi/picco/web/NoteBook.java
+++ src/main/java/tabitabi/picco/web/NoteBook.java
@@ -5,9 +5,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import tabitabi.picco.model.Note;
 
@@ -15,20 +17,47 @@ import tabitabi.picco.model.Note;
 @SessionScoped
 public class NoteBook implements Serializable{
   
-  private static final long serialVersionUID = 8967712157344853045L;
+  private static final long serialVersionUID = 8967712157344853046L;
 
   public static final String CREATE_NOTE_VIEW = "pm:editNoteView";
-  private final SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yy");
+  private static final String DEFAULT_DATE_FORMAT = "MMM/dd/yyyy";
+  private static final String DEFAULT_LOCALE = "en_US";
+  private static final String JAPANESE_DATE_FORMAT = "yyyy/MM/dd";
+  private static final String JAPANESE_LOCALE = "ja_JP";
+  private final SimpleDateFormat minDateFrmtr = new SimpleDateFormat("MM/dd/yy");
   private static final String MAX_DATE = "12/31/21";
   private Note note = new Note();
   private List<Note> notes = new ArrayList<Note>();
 
+  
+  public String getCalendarLocale(){
+    final Locale locale = FacesContext.getCurrentInstance().getViewRoot()
+        .getLocale();
+
+    if (Locale.JAPANESE.equals(locale)) {
+      return JAPANESE_LOCALE;
+    }
+    
+    return DEFAULT_LOCALE;   
+  }
+  
+  public String getDateFormat() {
+    final Locale locale = FacesContext.getCurrentInstance().getViewRoot()
+        .getLocale();
+
+    if (Locale.JAPANESE.equals(locale)) {
+      return JAPANESE_DATE_FORMAT;
+    }
+
+    return DEFAULT_DATE_FORMAT;
+  }
+  
   public String getMaxDate(){
     return MAX_DATE;
   }
   
   public String getMinDate(){
-    return dateFormatter.format(new Date());
+    return minDateFrmtr.format(new Date());
   }
   
   public Note getNote(){
